d990d5e90c42306d6219953177060c54419621f2
ines2357 <icatarinom@gmail.com>
2025-03-07 16:47:53 +0000

    Primeira página web

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
-
+from flask import Flask, render_template, request
 import fornecedores  
 import consumidor    
 
-# Função principal que gerencia o fluxo do programa
-def main():
-    # Chama o script do consumidor e permite que ele escolha o produto
-    produto_nome = consumidor.escolher_produto()
+app = Flask(__name__)
 
-    # Obter as pontuações do produto do fornecedor
+# Escolha do produto
+@app.route('/')
+def index():
+    # Lista de produtos 
+    produtos_disponiveis = ['Arroz', 'Trigo', 'Milho', 'Cevada']
+    return render_template('index.html', produtos=produtos_disponiveis)
+
+# Após a escolha
+@app.route('/escolher_produto', methods=['POST'])
+def escolher_produto():
+    
+    produto_nome = request.form['produto_nome']
+    
     pontuacoes_por_produto_localizacao = fornecedores.somar_pontuacoes_por_produto_localizacao()
 
-    # Filtra as pontuações relacionadas ao produto escolhido
     impactos = [(localizacao, pontuacao) for (produto, localizacao), pontuacao in pontuacoes_por_produto_localizacao.items() if produto == produto_nome]
     
     if not impactos:
-        print(f"Produto {produto_nome} não encontrado.")
-        return
-    
-    # Mostrar os resultados para o consumidor
-    print(f"\nImpacto ambiental do produto '{produto_nome}':")
-    for localizacao, impacto_total in impactos:
-        print(f"\n- Localização: {localizacao}")
-        print(f"  Impacto total: {impacto_total}")
+        return f"Produto {produto_nome} não encontrado.", 404
     
     # Determinar a localização com o menor impacto ambiental
     menor_impacto = min(impactos, key=lambda x: x[1])
     localizacao_menor_impacto, impacto_total = menor_impacto
     
-    print(f"\nO produto {produto_nome} com menor impacto ambiental corresponde ao local de produção {localizacao_menor_impacto}.")
-   
-# Execução do programa
+    # Resultados para a página de resultados
+    return render_template('resultado.html', produto=produto_nome, impactos=impactos, menor_impacto=(localizacao_menor_impacto, impacto_total))
+
+
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
 
 
