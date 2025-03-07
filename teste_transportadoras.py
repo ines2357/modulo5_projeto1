399e24a44de28cbe2871a69e1aca784d8dc0f5d8
tomas-fm <tomas.miranda.franco@gmail.com>
2025-03-07 15:00:19 +0000

    primeiro ficheiro teste das transportadoras

---
+++ teste_transportadoras.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def calcular_impacto_transporte():
+    origem = input("Origem do transporte: ")
+    
+    df = pd.read_excel('transportadoras.xlsx', sheet_name='transportadoras')
+    
+    for _, row in df.iterrows():
+        if row['Origem'] == origem:
+            origem, distancia, combustivel, co2 = (
+                row['Origem'],
+                float(row['Distância Percorrida (Km)']),
+                float(row['Combustível Total (L)']),
+                float(row['Emissões Totais (kg CO2)'])
+            )
+            
+            print("\n Impacto Ambiental do Transporte")
+            print(f"Origem: {origem}")
+            print(f"Distância: {distancia} km")
+            print(f"Combustível total consumido: {combustivel:.2f} L")
+            print(f"CO₂ total emitido: {co2:.2f} kg")
+            return
+    
+    print("\n Origem não encontrada!")
+
+if __name__ == "__main__":
+    calcular_impacto_transporte()
